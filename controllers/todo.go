a18dff8164365535d7890ff20f8beb67999dcfc7
Nikhil Jose <nikhiljose12345@gmail.com>
2023-04-17 23:49:42 +0530

    fix: read error handle, select specific fields in task list

--- controllers/todo.go
+++ controllers/todo.go
@@ -32,9 +32,18 @@ func AddTask(c *gin.Context){
 }
 
 func ListAll(c *gin.Context){
-	taskList := &[]models.Task{}
-	err:=config.DB.Find(taskList)
-	fmt.Println("err",err)
+	type ResData struct{
+		ID uint `json:"id"`
+		Title string `json:"title"`
+		Completed bool `json:"completed"`
+	}
+	var taskList []ResData	
+	err:=config.DB.Model(&models.Task{}).Find(&taskList).Error
+	if err!=nil{
+		c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{
+			"success":false,
+		})
+	}
 	c.JSON(http.StatusOK,gin.H{
 		"success":true,
 		"data":taskList,
